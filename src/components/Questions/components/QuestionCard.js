d0b27ce21a1b5b137077765be011b8320a71dcec
narpalan <thiagofumega@gmail.com>
2022-11-10 04:56:25 -0300

    BF - fix remove token from local storage.

--- src/components/Questions/components/QuestionCard.js
+++ src/components/Questions/components/QuestionCard.js
@@ -41,6 +41,7 @@ class QuestionCard extends React.Component {
     const { API_ER_CODE } = this.state;
     const res = await getQuestions(token);
     if (res.response_code === API_ER_CODE) {
+      localStorage.removeItem('token');
       const { history } = this.props;
       history.push('/');
     }
